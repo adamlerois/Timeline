0400e27a58db8455b7ab8f84c83ce3703323e99c
Youcef Bouhafna <bouhafnayoucef@gmail.com>
2016-02-24 19:31:25 -0700

    login signup
    implementing the action button tapped

--- Timeline/LoginSignupViewController.swift
+++ Timeline/LoginSignupViewController.swift
@@ -9,13 +9,78 @@
 import UIKit
 
 class LoginSignupViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    // computed property
+    var fieldsAreValid: Bool {
+        get {
+            switch mode {
+            case .Login:
+               return  !(emailTextField.text!.isEmpty  || passwordTextField.text!.isEmpty)
+            case .Signup:
+                return !(emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty || userNameTextField.text!.isEmpty)
+                
+                
+                }
+        
+                }
+            
+                }
+                
+                
+                
+    @IBAction func actionButtonTapped(sender: AnyObject) {
+        
+    }
+    
+        
+    
+    
+    
+    
+    
+    enum ViewMode {
+        case Login
+        case Signup
+    }
+    var mode: ViewMode = .Signup
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViewBasedOnMode()
     }
+        func updateViewBasedOnMode() {
+            switch mode {
+            case .Signup:
+                self.userNameTextField.hidden = false
+                self.passwordTextField.hidden = false
+                self.bioTextField.hidden = false
+                self.urlTextField.hidden = false
+                self.emailTextField.hidden = false
+                actionButton.setTitle("SignUp", forState: .Normal)
+            case .Login :
+                self.emailTextField.hidden = false
+                self.passwordTextField.hidden = false
+                self.userNameTextField.hidden = true
+                self.bioTextField.hidden = true
+                self.urlTextField.hidden = true
+                actionButton.setTitle("LogIn", forState: .Normal)
+            }
+            
+        }
 
+        
+    
+
+    @IBAction func actionButton(sender: AnyObject) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
